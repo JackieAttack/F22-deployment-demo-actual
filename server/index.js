082976d5e7ac3e6d449491c85a39823604c0b8c7
Jackie Bearnson <Jackie.bearnson@gmail.com>
2022-05-18 14:41:53 -0600

    Testing git functionality

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const path = require("path");
 
 const app = express();
 
+//Let's build the path to our front end files so Heroku can access them.
+//middleware
+// app.use(express.static(path.join(__dirname, "../client")));
+
 const port = process.env.PORT || 4005; //process.env.PORT refers to herokus port number, not ours. the or 4005 means our local port if it's not deployed by heroku
 
 //Let's build the path to our front end files so Heroku can access them.
@@ -11,6 +15,11 @@ const port = process.env.PORT || 4005; //process.env.PORT refers to herokus port
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/index.html"))
 })
+
+app.get("/styles", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/index.css"))
+})
+
 
 /*
 wHEN hEROKU DEPLOYS OUR APP, IT WILL PROVIDE ITS OWN port number.
